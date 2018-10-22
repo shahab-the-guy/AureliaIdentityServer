817df155b1e1a0033752b7a9c07ddb4cd90d0551
Saeed Ganji <shahabganji@gmail.com>
2018-10-22 12:34:59 +0330

    refactor(before publish)

--- Aurelia.Client/src/routes/auth/login.ts
+++ Aurelia.Client/src/routes/auth/login.ts
@@ -6,21 +6,9 @@ import { autoinject } from 'aurelia-framework';
 @autoinject()
 export class Login {
 
-  constructor(private openIdConnect: OpenIdConnect) {
-
-  }
+  constructor(private openIdConnect: OpenIdConnect) { }
 
   private login() {
-    // return new Promise((resolve, reject) => {
-    //   console.log('Log');
     this.openIdConnect.login();
-    //   .then(_ => {
-    //     resolve();
-    //   }).catch(_ => {
-    //     alert('Login failed');
-    //     reject();
-    //   });
-    // });
   }
-
 }
--- Aurelia.Client/src/routes/home/home.ts
+++ Aurelia.Client/src/routes/home/home.ts
@@ -6,7 +6,6 @@ import { HttpClient } from "aurelia-http-client";
 export class Home {
 
   private access_token;
-
   private frameworks: Array<any>;
 
   constructor(private openIdConnect: OpenIdConnect, private httpClient: HttpClient) { }
@@ -17,7 +16,8 @@ export class Home {
     this.httpClient.configure(config => {
       config.withBaseUrl("https://localhost:44346/")
         .withHeader('Accept', 'application/json')
-        .withHeader('Authorization', `Bearer ${this.access_token}`); // adds the access token, so that we can call secure apis
+        // adds the access token, so that we can call secure apis
+        .withHeader('Authorization', `Bearer ${this.access_token}`); 
     });
 
     return this.httpClient.get('api/secure')
@@ -27,10 +27,8 @@ export class Home {
 
   }
 
-
   private logout() {
     this.openIdConnect.logout();
   }
-
 
 }
